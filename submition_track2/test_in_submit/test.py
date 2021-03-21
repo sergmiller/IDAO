25bd95c5a8f9686a4249dd71489bd92eb613122d
Serg Miller <sergey.miller@phystech.edu>
2021-03-21 20:25:56 +0300

    Update test.py

--- submition_track2/test_in_submit/test.py
+++ submition_track2/test_in_submit/test.py
@@ -17,8 +17,9 @@ print(np.mean(res), np.std(res))
 
 import time
 start = time.time()
-for i in np.arange(10000):
-    t = np.random.random((1,3,500,500))
-    res = mobilenet(torch.Tensor(t)).detach().numpy()
+for i in np.arange(100):
+    t = np.random.random((32,3,500,500))
+    with torch.no_grad():
+        res = mobilenet(torch.Tensor(t)).detach().numpy()
     
 print('OK, run {}s'.format(time.time() - start))
