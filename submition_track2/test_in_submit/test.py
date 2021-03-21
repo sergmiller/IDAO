34ef5b9d14a4f6ff3459ee2af2c5f9e675af59ed
sergmiller <sergey.miller@phystech.edu>
2021-03-21 20:13:10 +0300

    add test for track2

---
+++ submition_track2/test_in_submit/test.py
@@ -0,0 +1,24 @@
+import numpy as np
+import torch
+import torchvision
+
+mobilenet = torchvision.models.mobilenet_v3_small()
+mobilenet.load_state_dict(torch.load("mobilenet_state_dict"))
+
+np.random.seed(0)
+t = np.random.random((1,3,500,500))
+model2.train(False)
+print(np.mean(t), np.std(t))
+with torch.no_grad():
+    res = mobilenet(torch.Tensor(t)).detach().numpy()
+print(np.mean(res), np.std(res))
+
+
+
+import time
+start = time.time()
+for i in np.arange(10000):
+    t = np.random.random((1,3,500,500))
+    res = mobilenet(torch.Tensor(t)).detach().numpy()
+    
+print('OK, run {}s'.format(time.time() - start))
