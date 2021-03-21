c0bce89c53cdf190790136b6bf00f2237afae538
Serg Miller <sergey.miller@phystech.edu>
2021-03-21 21:04:21 +0300

    Update test.py

--- submition_track2/test_in_submit/test.py
+++ submition_track2/test_in_submit/test.py
@@ -7,7 +7,7 @@ mobilenet.load_state_dict(torch.load("mobilenet_state_dict"))
 
 np.random.seed(0)
 t = np.random.random((1,3,500,500))
-model2.train(False)
+mobilenet.train(False)
 print(np.mean(t), np.std(t))
 with torch.no_grad():
     res = mobilenet(torch.Tensor(t)).detach().numpy()
