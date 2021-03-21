1552fee63663f31f9519ba600ef91695b45562ed
AlcibiadesCleinias <ivan.cheprasov@phystech.edu>
2021-03-22 01:05:45 +0300

    fix

--- submition_track2/test_in_submit/test_run.py
+++ submition_track2/test_in_submit/test_run.py
@@ -12,11 +12,11 @@ for i in np.arange(100):
 import pandas as pd
 import sys
 sys.appned('../..')
-from utilz import file, dataset
+from utils import file, dataset
 PATH_TO_TEST_DIRS = 'tests/'
 d1 = file.read_all_png_in_dir(PATH_TO_TEST_DIRS)
 res = []
-for key in dataset._fix_tags(read_all_png_in_dir(path).keys()):
+for key in dataset._fix_tags(d1.keys()):
     res.append([key, 0, 0])
 data_frame = pd.DataFrame(res, columns=["id", "classification_predictions", "regression_predictions"])
 data_frame.to_csv('submission.csv', index=False, header=True)
