60acbdecbb232015ea729ae0b22b718b71cf3ede
AlcibiadesCleinias <ivan.cheprasov@phystech.edu>
2021-03-22 01:03:48 +0300

    norm?

--- submition_track2/test_in_submit/test_run.py
+++ submition_track2/test_in_submit/test_run.py
@@ -8,3 +8,15 @@ for i in np.arange(100):
     t = np.random.random((32,3,500,500))
     with torch.no_grad():
         res = mobilenet(torch.Tensor(t)).detach().numpy()
+
+import pandas as pd
+import sys
+sys.appned('../..')
+from utilz import file, dataset
+PATH_TO_TEST_DIRS = 'tests/'
+d1 = file.read_all_png_in_dir(PATH_TO_TEST_DIRS)
+res = []
+for key in dataset._fix_tags(read_all_png_in_dir(path).keys()):
+    res.append([key, 0, 0])
+data_frame = pd.DataFrame(res, columns=["id", "classification_predictions", "regression_predictions"])
+data_frame.to_csv('submission.csv', index=False, header=True)
