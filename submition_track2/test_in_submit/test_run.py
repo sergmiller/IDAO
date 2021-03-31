1f2438a621cfc84cf72c759ae282fce21dc03739
sergmiller <sergey.miller@phystech.edu>
2021-03-31 19:30:41 +0300

    flush

--- submition_track2/test_in_submit/test_run.py
+++ submition_track2/test_in_submit/test_run.py
@@ -59,40 +59,67 @@ def mae_scorer(estimator, X, y):
     pass  # need this declaration to upload model from pickle
 
 
-class ModelWithProjection:
-    def __init__(self, model, side):
-        self.model = model
-        self.side = side
-    def predict(self, x):
-        x_proj = apply_random_projection_and_normalize(x, to_dim=64)
-        return self.model.predict(x_proj)
-    def predict_proba(self, x):
-        x_proj = apply_random_projection_and_normalize(x, to_dim=64)
-        return self.model.predict_proba(x_proj)[:, self.side]
+class TrainLightModel:
+    def __init__(self, energy_model, particle_model):
+        self.energy_model = energy_model
+        self.particle_model = particle_model
+
+    def predict_particle(self, x):
+        x = apply_random_projection_and_normalize(x, to_dim=64)
+        return self.particle_model.predict_proba(x)[:, 0]
+
+    def predict_energy(self, x):
+        x = apply_random_projection_and_normalize(x, to_dim=64)
+        return self.energy_model.predict(x)
+
+
+class TestLightModel:
+    # X is for 1 and 6, also 30 have 2 clusters
+    CLUSTER_TO_ENERGY = ['X', '20', '30', '3', '10', '30']
+    CLUSTER_1_6_MEDIAN = 0.8917
+    CLASSES = ['1', '10', '20', '3', '30', '6']  # alphabet order
+    CLASS2ID = {c:i for i,c in enumerate(CLASSES)}
+    def __init__(self, energy_model, cluster_model):
+        self.energy_model = energy_model
+        self.cluster_model = cluster_model
+
+    def predict_particle(self, x):
+        x = apply_random_projection_and_normalize(x, to_dim=64)
+        probs = self.energy_model.predict_proba(x)
+        probs_zero = probs[:, self.CLASS2ID['1']] \
+            + probs[:, self.CLASS2ID['6']] + probs[:, self.CLASS2ID['20']]
+        return probs_zero
+
+    def predict_energy(self, x):
+        x = apply_random_projection_and_normalize(x, to_dim=64)
+        clusters = self.cluster_model.predict(x)
+        probs = self.energy_model.predict_proba(x)[:, 0]  # 0 is for '1' energy
+        is_energy_class_1 = probs > self.CLUSTER_1_6_MEDIAN
+        result = np.array([self.CLUSTER_TO_ENERGY[x] for x in clusters])
+        for i, c in enumerate(result):
+            if c != "X":
+                continue
+            result[i] = '1' if is_energy_class_1[i] else '6'
+        return result
+
 
 import pickle
 
-with open('models/cv_label1.pkl', 'rb') as f:
+with open('models/cv_label1_mobilenet_crop_64.pkl', 'rb') as f:
     cv_label1 = pickle.load(f)
 
-with open('models/cv_label2.pkl', 'rb') as f:
+with open('models/cv_label2_mobilenet_crop_64.pkl', 'rb') as f:
     cv_label2 = pickle.load(f)
 
-
-with open('models/cv_label1_tsne_test.pkl', 'rb') as f:
-    cv_label1_test = pickle.load(f)
-
-with open('models/cv_label2_tsne_test.pkl', 'rb') as f:
-    cv_label2_test = pickle.load(f)
+with open('models/cv_label2_tsne_test_v4.pkl', 'rb') as f:
+    cv_cluster_model = pickle.load(f)
 
 
-model1 = ModelWithProjection(cv_label1, 0)
-model2 = ModelWithProjection(cv_label2, 0)
-model1_test = ModelWithProjection(cv_label1_test, 1)
-model2_test = ModelWithProjection(cv_label2_test, 1)
+train_model = TrainLightModel(cv_label2, cv_label1)
+test_model = TestLightModel(cv_label2, cv_cluster_model)
 
 
-def pipe_for_dir(dir : dir, m1, m2) -> dataset.LabeledDataset:
+def pipe_for_dir(dir : dir, light_model) -> dataset.LabeledDataset:
     return pipe.apply_all_models_to_test_dataset_gen(
         dataset.build_batched_dataset(
             [dir],
@@ -100,8 +127,7 @@ def pipe_for_dir(dir : dir, m1, m2) -> dataset.LabeledDataset:
             # limit=256,
             batch_size=48),
         croped_mobilenet,
-        m1,
-        m2
+        light_model
     )
 
 
@@ -111,8 +137,8 @@ from utils import file
 # PATH_TO_TEST_DIRS = "../../data/idao_dataset"
 PATH_TO_TEST_DIRS = os.path.abspath('./tests')
 
-public_predictions = pipe_for_dir(os.path.join(PATH_TO_TEST_DIRS, 'public_test'), model1, model2)
-private_predictions = pipe_for_dir(os.path.join(PATH_TO_TEST_DIRS, 'private_test'), model1_test, model2_test)
+public_predictions = pipe_for_dir(os.path.join(PATH_TO_TEST_DIRS, 'public_test'), train_model)
+private_predictions = pipe_for_dir(os.path.join(PATH_TO_TEST_DIRS, 'private_test'), test_model)
 
 submit = dataset.LabeledDataset.merge(public_predictions, private_predictions)
 # print(public_predictions.labels)
--- utils/pipe.py
+++ utils/pipe.py
@@ -15,16 +15,15 @@ from .applier import build_embd_dataset
 def apply_all_models_to_test_dataset_gen(
     dataset_gen,
     backbone,
-    model1,
-    model2
+    light_model
 ) -> LabeledDataset:
 
     labels = []
     tags = []
     for batch in dataset_gen:
         emb_batch_dataset = build_embd_dataset(batch, backbone, batch_size=len(batch.samples), n_jobs=1)
-        labels1 = model1.predict_proba(emb_batch_dataset.samples)
-        labels2 = model2.predict(emb_batch_dataset.samples)
+        labels1 = light_model.predict_particle(emb_batch_dataset.samples)
+        labels2 = light_model.predict_energy(emb_batch_dataset.samples)
         labels_batch = [[l1,l2] for l1, l2 in zip(labels1, labels2)]
         labels.extend(labels_batch)
         tags.extend(emb_batch_dataset.tags)
@@ -41,8 +40,7 @@ def apply_all_models_to_test_dataset_gen(
 def apply_all_models_to_test_dataset(
     d : LabeledDataset,
     backbone,
-    model1,
-    model2,
+    light_model,
     key : str,
     _emb_cache={}
 ) -> LabeledDataset:
@@ -50,8 +48,8 @@ def apply_all_models_to_test_dataset(
         _emb_cache[key] = build_embd_dataset(d, backbone)
     emb_dataset = _emb_cache.get(key)
 
-    labels1 = model1.predict_proba(emb_dataset.samples)[:, 0]
-    labels2 = model2.predict(emb_dataset.samples)
+    labels1 = light_model.predict_particle(emb_dataset.samples)
+    labels2 = light_model.predict_energy(emb_dataset.samples)
 
     labels = np.stack([labels1, labels2]).T
 
